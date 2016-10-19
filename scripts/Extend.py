291f91c958000567c089df3506c54cf1155f8366
Julien Fouret <julien.fouret12@uniagro.fr>
2016-10-19 16:29:23 +0200

    add multispecies !

--- scripts/Extend.py
+++ scripts/Extend.py
@@ -9,9 +9,11 @@ contact='julien@fouret.me'
 parser = argparse.ArgumentParser(description='Take an region multialignment file and add one speacies from bam file',epilog="Version : "+str(version)+"\n"+str(year)+"\nAuthor : "+author+" for more informations or enquiries please contact "+contact,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-outDir', metavar='/path', required=True, help="Output directory")
 parser.add_argument('-aln', metavar='/path', required=True, help="Alignment to extend")
-parser.add_argument('-bam', metavar='/path', required=True, help="bam file")
 parser.add_argument('-queue', metavar='queue', required=False, help="queue for PBS",default='batch')
-parser.add_argument('-ref', metavar='/path', required=True, help="reference fasta file used for alignment")
+parser.add_argument('-refs', metavar='/path', required=True, help="references to be used csv file with id;fasta;bam in the order of priority  \n"+
+	"id: the id of the species used in the exon alignment file\n"+
+	"fasta: path to the fasta file used for short read alignments\n"+
+	"bam: path of the sorted and indexed bam file")
 parser.add_argument('-spec', metavar='name', required=True, help="name of the species to add")
 args=parser.parse_args()
 
@@ -29,45 +31,103 @@ rootedDir=RootDir(args.outDir,pbs=True)
 rootedDir.logs.writeArgs(args)
 
 # definition of used software
+samtools=Command('samtools',min='1.3')
+samtools.versionCtrl()
+samtools.log()
+java=Command('java')
+java.log()
+picard_cmd=java.create(options={'-jar':args.picard})
+picard=Command(picard_cmd,picard_cmd+' CheckFingerprint --version 2>&1 | sed \'s/(.*$//g\'')
+picard.log()
 bam2consensus=gitCommand(Git(gitRepository),'bam2consensus.py','bin')
 bam2consensus.log()
 
 #Getting absolute path
 alnFileName=os.path.abspath(args.aln)
 bamFileName=os.path.abspath(args.bam)
-refFileName=os.path.abspath(args.ref)
+refsFileName=os.path.abspath(args.refs)
 
 #Define var
-reID=re.compile('^(uc\w*)(\.\d*)_hg19_(\d*)_(\d*) (\d*) (\d*) (\d*) (\w+:\d+-\d+)([+-])$')
+reID=re.compile('^(uc\w*)(\.\d*)_(\w*)_(\d*)_(\d*) (\d*) (\d*) (\d*)\s?(\w+:\d+-\d+[+-]{1})?$')
 #Group
 #kgID= 1+2
-#exon num = 3 
-#exon tot = 4
-#reg = 8
-#strand = 9
+#spec= 3
+#exon num = 4
+#exon tot = 5
+#reg and strand = 9
 
 # configure batch
 Nlim=80
 N=0
 iterBatch=1
+index=0
+
+# extract info from refFile
+refDict=dict() # stock the regions + strands for each specie 
+specOrder=list() # stock the order of the species
+with open(args.reference,'r') as refFile:
+	for line in refFile.readlines():
+		line=line.rstrip()
+		spec,fasta,bam=line.split(';')
+		refDict[spec]=''
+
+		#TODO fai index ???
+		#TODO dict index ???.
+
+
+		## step 3.0 - check for the presence of indexation files
+
+		prefixRef=fasta.replace('.fa','')
+		prefixRef=prefixRef.replace('.fna','')
+		prefixRef=prefixRef.replace('.fasta','')
+
+		faiRef=fasta+'.fai'
+		dictRef=prefixRef+'.dict'
+
+		if not os.path.isfile(dictRef):
+			dictOpt={
+				'REFERENCE':ref,
+				'OUTPUT':dictRef
+			}
+			submitOneShell(picard.create(options=dictOpt,sep='=',subprogram='CreateSequenceDictionary'))
+
+		if not os.path.isfile(faiRef):
+			faiPos=[ref]
+			submitOneShell(samtools.create(positionals=faiPos,subprogram='faidx'))
+
+
+		#TODO check if spec names are corect(matching those in the multi aln file)
+		#TODO check the presence of hg19
 
 fasta_sequences = SeqIO.parse(open(alnFileName),'fasta')
 # step 1 - iterate over the aln File to compute consensus seq
 cmdList=list()
+NbSpec=0 # 0 species matched yet
+NbSpecLim=len(refDict.keys()) # number of species to match before to start searching consensus
 for fasta in fasta_sequences:
 	name, sequence = fasta.description, str(fasta.seq)
 	m=reID.match(name)
 	#time.sleep(0.05)#debug
 	#print(name)#debug
 	if m:
-		#time.sleep(0.5)#debug
+		if m.group(9)==None:
+			region=''
+		else:
+			region=m.group(9)
+		refDict[m.group(3)]=m.group(9)
+		NbSpec=+1
+	if NbSpec==NbSpecLim:
+		#time.sleep(0.05)#debug
 		#print(name)#debug
-		consensusOutPut=rootedDir.results+'/'+m.group(1)+m.group(2)+'/exon'+m.group(3)
+		consensusOutPut=rootedDir.results+'/'+m.group(1)+m.group(2)+'/exon'+m.group(4)
+		regionOrdList=list()
+		for spec in specOrder:
+			regionOrdList.append(refDict(spec))
+		regions=';'.join(regionOrdList)# regions != region
 		bam2consensusOpt={
 			'-outDir':consensusOutPut,
-			'-bam':bamFileName,
-			'-ref':refFileName,
-			'-reg':m.group(8)
+			'-reference':refsFileName,
+			'-reg':regions
 		}
 		cmdList.append(bam2consensus.create(wd=rootedDir.results,options=bam2consensusOpt))
 		N+=1
@@ -95,16 +155,19 @@ for fasta in fasta_sequences:
 		pos=[int(pos[0]),int(pos[1])]
 		seqLen=max(pos)-min(pos)+1
 		consensusOutPut=rootedDir.results+'/'+m.group(1)+m.group(2)+'/exon'+m.group(3)
-		while not os.path.exists(consensusOutPut+'/fileRooting.pkl'):
+		while not os.path.exists(consensusOutPut+'/fileRooting.pkl'): # infinite loop if bug . . . (sol ? write an error file in error ? through def main: and main())
 			time.sleep(10)
-		header='>'+m.group(1)+m.group(2)+'_'+args.spec+'_'+m.group(3)+'_'+m.group(4)+' '+m.group(5)+' '+m.group(6)+' '+m.group(7)+' '+m.group(8)+m.group(9)+" BAM:BAM-BAM\n"
+		header='>'+m.group(1)+m.group(2)+'_'+args.spec+'_'+m.group(4)+'_'+m.group(5)+' '+m.group(6)+' '+m.group(7)+' '+m.group(8)+' '+m.group(9)+m.group(9)+" BAM:BAM-BAM\n"
 		outAln.write(header)
 		with open(consensusOutPut+'/results/hg19mapped.fa','r') as consensusFile:
 			seq=''.join(consensusFile.readlines()[1:]).replace("\n",'')
 			if len(seq)!=seqLen:
 				print('ERROR SEQ SIZE for '+name)
 				sys.exit(1)
-		if strand=='-':
-			seq = str(Seq(seq).reverse_complement())
 		outAln.write(seq+"\n")
 outAln.close()
+
+# finish and saveRoot
+saveRoot(rootedDir)
+
+sys.exit(0)
--- scripts/bam2consensus.py
+++ scripts/bam2consensus.py
@@ -8,13 +8,14 @@ contact='julien@fouret.me'
 
 parser = argparse.ArgumentParser(description='Retrive consensus sequence of a given region based on a sorted bam file',epilog="Version : "+str(version)+"\n"+str(year)+"\nAuthor : "+author+" for more informations or enquiries please contact "+contact,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-outDir', metavar='/path', required=True, help="Output directory")
-parser.add_argument('-bam', metavar='/path', required=True, help="A sorted bam file")
-parser.add_argument('-ref', metavar='/path', required=True, help="the reference associated to this bam file")
-parser.add_argument('-reg', metavar='chr:start-end', required=True, help="region of interest, please respect the format")
+parser.add_argument('-reference', metavar='/path', required=True, help="csv file with id;fasta;bam in the order of priority  \n"+
+	"id: the id of the species used in the exon alignment file\n"+
+	"fasta: path to the fasta file used for short read alignments\n"+
+	"bam: path of the sorted and indexed bam file")
+parser.add_argument('-reg', metavar='chr:start-end+;chr:start-end-;...', required=True, help="region of interest, please respect the format and the order of the ref file")
 parser.add_argument('-gatk', metavar='/path', required=False, help="gatk jar path",default='/export/bin/source/GenomeAnalysisTK/GenomeAnalysisTK.jar')
 parser.add_argument('-picard', metavar='/path', required=False, help="picard jar path",default='/export/bin/picard-tools-2.1.0/picard.jar')
 parser.add_argument('-graphCov', action='store_true', help="Graph the coverage")
-parser.add_argument('-strict', action='store_true', help="do not consid")
 parser.add_argument('-hg19', metavar='/path', required=False, help="hg19 genome for base mapping",default='/export/data/Genomes/Human/hg19/hg19.fa')
 parser.add_argument('-hg19Dict', metavar='/path', required=False, help="hg19 genome for base mapping",default='/export/data/Genomes/Human/hg19/hg19.dict')
 args=parser.parse_args()
@@ -22,6 +23,7 @@ args=parser.parse_args()
 # Import libraries
 import sys
 import os
+from Bio import SeqIO
 from jupype import *
 
 # Create output directory structure and logs
@@ -38,72 +40,85 @@ bcftools.log()
 java=Command('java')
 java.log()
 gatk_cmd=java.create(options={'-jar':args.gatk})
-gatk=Command(gatk_cmd,gatk_cmd+' -version 2>&1',min='3.6')
-gatk.versionCtrl()
+gatk=Command(gatk_cmd,gatk_cmd+' -version 2>&1')
 gatk.log()
 megacc=Command('megacc')
+megacc.log()
 picard_cmd=java.create(options={'-jar':args.picard})
 picard=Command(picard_cmd,picard_cmd+' CheckFingerprint --version 2>&1 | sed \'s/(.*$//g\'')
 picard.log()
 
 #get absolute path from inputs
-bam=os.path.abspath(args.bam)
-ref=os.path.abspath(args.ref)
-
+regions=args.reg.split(';')
+index=0
+refDict=dict()
+specOrder=list() # priority of species (closely related ?)
+with open(args.reference,'r') as refFile:
+	for line in refFile.readlines():
+		line=line.rstrip()
+		spec,fasta,bam=line.split(';')
+		region=regions[index]
+		if region!='':
+			refDict[spec]={
+				'fasta':fasta,
+				'bam':bam,
+				'reg':region,
+			}
+		specOrder.append(spec)
 os.chdir(rootedDir.results)
-## step 1 - Creation a vcf-formatted files for mpileup
 
-#define options and positional args for software
-mpileupOpt={
-	'-r':args.reg,
-	'--reference':ref,
-	'-v':'',
-	'-o':rootedDir.results+'/tmp.vcf.gz'
-	}
-mpileupPos=[bam]
+## step 1 - iteration for all species
+NbCallsDict=dict()
+for spec in refDict.keys():
+	## step 1.1 - Creation a vcf-formatted files for mpileup - base calling
+	mkdirp(spec)
+	#define options and positional args for software
+	mpileupOpt={
+		'-r':refDict[spec][reg][:-1], # [:-1] without strand information yet ...
+		'--reference':efDict[spec][fasta],
+		'-v':'',
+		'-o':rootedDir.results+'/'+spec+'/call.vcf.gz'
+		}
+	mpileupPos=[bam]
 
-submitOneShell(samtools.create(options=mpileupOpt,positionals=mpileupPos,subprogram='mpileup'))
+	submitOneShell(samtools.create(options=mpileupOpt,positionals=mpileupPos,subprogram='mpileup'))
 
-## step 2 - call consensus at vcf format including variant and non-variant
+	## step 1.2 - call genotype at vcf format including variant and non-variant
 
-callOpt={
-	'-c':'',
-	'-M':'',
-	'-O':'v',
-	'-o':rootedDir.results+'/tmp_calling.vcf'
-	}
-callPos=[rootedDir.results+'/tmp.vcf.gz']
+	callOpt={
+		'-c':'',
+		'-M':'',
+		'-O':'v',
+		'-o':rootedDir.results+'/'+spec+'/genotype.vcf'
+		}
+	callPos=[rootedDir.results+'/'+spec+'/call.vcf.gz']
 
-submitOneShell(bcftools.create(options=callOpt,positionals=callPos,subprogram='call'))
-submitOneShell("grep -v -e \"\tINDEL\t\" "+rootedDir.results+'/tmp_calling.vcf > '+rootedDir.results+'/calling.vcf')
-submitOneShell("rm "+rootedDir.results+'/tmp_calling.vcf '+rootedDir.results+'/calling.vcf.idx')
+	submitOneShell(bcftools.create(options=callOpt,positionals=callPos,subprogram='call'))
 
-## step 3.0 - check for the presence of indexation files
+	submitOneShell("rm "+rootedDir.results+'/'+spec+'/genotype.vcf.idx')
 
-prefixRef=ref.replace('.fa','')
-prefixRef=prefixRef.replace('.fna','')
-prefixRef=prefixRef.replace('.fasta','')
+	## step 1.3 - check the spe
+	NbCallsDict[spec]=int(submitOneShell("grep -c -v -P '^#' "+rootedDir.results+'/'+spec+'/genotype.vcf')['out'].rstrip())
 
-faiRef=ref+'.fai'
-dictRef=prefixRef+'.dict'
+## step 2 - choose the to create the consensus based on the species with the most positions called
 
-if not os.path.isfile(dictRef):
+chosenSpec=None
+for spec in NbCallsDict.keys():
+	if chosenSpec==None:
+		chosenSpec=spec
+		maxNbCall=NbCallDict[spec]
+	elif NbCallDict[spec]>maxNbCall:
+		chosenSpec=spec
+		maxNbCall=NbCallDict[spec]
+	elif NbCallDict[spec]==maxNbCall:
+		if specOrder.index(chosenSpec)>specOrder.index(spec):
+			chosenSpec=spec
+			maxNbCall=NbCallDict[spec]
 
-	dictOpt={
-		'REFERENCE':ref,
-		'OUTPUT':dictRef
-	}
-	submitOneShell(picard.create(options=dictOpt,sep='=',subprogram='CreateSequenceDictionary'))
-
-if not os.path.isfile(faiRef):
-	faiPos=[ref]
-	submitOneShell(samtools.create(positionals=faiPos,subprogram='faidx'))
-
-## step 3.0 - check variant calling
-NbCalls=submitOneShell("grep -c -v -P '^#' "+rootedDir.results+'/calling.vcf')['out'].rstrip()
+NbCalls=NbCallDict[chosenSpec]
 
 ## step 3.1 - create consensus
-if NbCalls=='0':
+if NbCalls==0:
 	import re
 	with open(rootedDir.results+'/hg19mapped.fa','w') as csFile:
 		csFile.write('>1 '+args.reg.split('-')[0]+"\n")
@@ -115,9 +130,9 @@ else:
 	gatkOpt={
 		'-T':'FastaAlternateReferenceMaker',
 		'-R':ref,
-		'-o':rootedDir.results+'/consensus.fa',
+		'-o':rootedDir.results+'/'+chosenSpec+'/tmp_consensus.fa',
 		'-L':args.reg,
-		'-V':rootedDir.results+'/calling.vcf',
+		'-V':rootedDir.results+'/'+chosenSpec+'/genotype.vcf',
 		'-U':'ALLOW_SEQ_DICT_INCOMPATIBILITY'
 	}
 	submitOneShell(gatk.create(options=gatkOpt))
@@ -129,27 +144,39 @@ else:
 	}
 	
 	submitOneShell('cp '+args.hg19Dict+' '+rootedDir.results+'/regions.txt')
-	chromosome,positions=args.reg.split(':')
-	start,end=positions.split('-')
+	chromosome,positions=refDict['hg19']['reg'].split(':')
+	start,end=positions[:-1].split('-')
+	strand=positions[-1]
 	#start=str(int(start)+1)
 	with open(rootedDir.results+'/regions.txt','a') as regFile:
-		regFile.write("\t".join([chromosome,start,end,'+','.'])+"\n")
+		regFile.write("\t".join([chromosome,start,end,strand,'.'])+"\n")
 	submitOneShell(picard.create(options=getRefOpt,subprogram='ExtractSequences',sep='='))
-	submitOneShell('cat '+rootedDir.results+'/hg19.fa '+rootedDir.results+'/consensus.fa > '+rootedDir.results+'/data.fa')
+	if strand=='-':
+		#reverse complement
+		fasta_sequences=SeqIO.parse(open(rootedDir.results+'/'+chosenSpec+'/tmp_consensus.fa'),'fasta')
+		for fasta in fasta_sequences:
+			name, sequence = fasta.description, str(fasta.seq.reverse_complement())
+			with open(rootedDir.results+'/'+chosenSpec+'/consensus.fa') as consFile:
+				consFile.write('>'+name+"\n"+sequence+"\n")
+		os.remove(rootedDir.results+'/'+chosenSpec+'/tmp_consensus.fa')
+	else:
+		# or just rename if strand +
+		os.rename(rootedDir.results+'/'+chosenSpec+'/tmp_consensus.fa',rootedDir.results+'/'+chosenSpec+'/consensus.fa')
+
+	submitOneShell('cat '+rootedDir.results+'/hg19.fa '+rootedDir.results+'/'+chosenSpec+'/consensus.fa > '+rootedDir.results+'/'+chosenSpec+'/data.fa')
 
 	## step 3.2.1 Build an alignment consensus-hg19 in aln.fasta
 	megaccOpt={
 		'-a':gitRepository+'/template/clustal_align_nucleotide.mao',
-		'-d':rootedDir.results+'/data.fa',
-		'-o':rootedDir.results+'/aln',
+		'-d':rootedDir.results+'/'+chosenSpec+'/data.fa',
+		'-o':rootedDir.results+'/'+chosenSpec+'/aln',
 		'-f':'Fasta'
 	}
 	submitOneShell(megacc.create(options=megaccOpt))
 
 	## step 3.2.2 parse the alignment to prepare to hg19 mapped exon (no gap in hg19)
-	from Bio import SeqIO
 	seqList=list()
-	fasta_sequences=SeqIO.parse(open(rootedDir.results+'/aln.fasta'),'fasta')
+	fasta_sequences=SeqIO.parse(open(rootedDir.results+'/'+chosenSpec+'/aln.fasta'),'fasta')
 	for fasta in fasta_sequences:
 		name, sequence = fasta.description, str(fasta.seq)
 		seqList.append(sequence)
@@ -163,7 +190,7 @@ else:
 		mappedFile.write('>hg19mapped '+args.reg+"\n")
 		mappedFile.write(mappedSeq+"\n")
 
-	## step 4.0 - Create coverage.tab file for graph outputs
+	## step 4.0 - Create coverage.tab file for graph outputs TODO ==> put in a seperate programe just with outDir
 
 	if args.graphCov:
 		submitOneShell('echo "Position'+"\t"+'Reference'+"\t"+'Alternative'+"\t"+'Depth'+"\t"+'Genotype"'+' > coverage.tab ; grep -P "^#" -v calling.vcf |grep -e \'INDEL\' -v | awk -F "[\t ;:]" \'{print $2 "\t" $4 "\t" $5 "\t" $8 "\t" $(NF-1) }\' | sed \'s/DP=//g\' >> coverage.tab')
