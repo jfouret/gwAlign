86b581e7518490dfceb4665dfd099416a7411950
Julien Fouret <julien.fouret12@uniagro.fr>
2016-10-24 11:57:57 +0200

    debug

--- scripts/bam2consensus.py
+++ scripts/bam2consensus.py
@@ -52,6 +52,8 @@ megacc.log()
 picard_cmd=java.create(options={'-jar':args.picard})
 picard=Command(picard_cmd,picard_cmd+' CheckFingerprint --version 2>&1 | sed \'s/(.*$//g\'')
 picard.log()
+bedtools=command('bedtools',"bedtools --version | sed -r 's/bedtools v(.*)/\1/g'")
+bedtools.log()
 
 #get absolute path from inputs
 regions=args.reg.split(',')
